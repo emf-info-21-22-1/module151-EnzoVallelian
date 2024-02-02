b05ffe5e5cd588946a634afbb83bdcb24df122ea
EnzoVallelian <enzo.vallelian@gmail.com>
2024-02-02 08:23:00 +0100

    Exo 3 fini

--- exercices/exercice3/equipes.php
+++ exercices/exercice3/equipes.php
@@ -1,21 +1,34 @@
 <!doctype html>
 <html>
-  <header>
-    <link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
+<header>
+  <link rel="stylesheet" type="text/css" href="stylesheets/main.css" />
 </header>
-  <body>
-    <div id="conteneur">
-      <h1>Les équipes de National League</h1>    
-      <table border= "1">
+
+<body>
+  <div id="conteneur">
+    <h1>Les équipes de National League</h1>
+    <table border="1">
       <tr>
         <td>ID</td>
         <td>Club</td>
       </tr>
       <?php
-        require('ctrl.php');
-        
+      require('ctrl.php');
+      $id = 1 ;
+      $valeurInput = getEquipes();
+      foreach ($valeurInput as $valeur) {
+      echo "<tr>";
+      echo "<td>".$id."</td>";
+      echo "<td> ". $valeur ."</td>";
+      echo "</tr>";
+      $id++;
+      }
+
+
+
       ?>
-      </table>
-    </div>
-  </body>
+    </table>
+  </div>
+</body>
+
 </html>
