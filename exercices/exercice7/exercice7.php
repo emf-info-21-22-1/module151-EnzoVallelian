83b6dacfa6d45e30a003d7b7502946829fcb3185
EnzoVallelian <enzo.vallelian@gmail.com>
2024-02-02 13:42:59 +0100

    exo 7 fini

--- exercices/exercice7/exercice7.php
+++ exercices/exercice7/exercice7.php
@@ -1,14 +1,21 @@
 <?php
-	$bdd = new PDO('mysql:host=localhost;dbname=nomDB', 'root', 'pwd');
-	$reponse = …;
-	
-	
-	while (…)
-	{
-	
-	        …
+try {
+	$bdd = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'emf123');
+} catch (PDOException $e) {
+	die('Erreur de connexion : ' . $e->getMessage());
+}
 
-	
-	}
-	$reponse->closeCursor();
+// Exemple de requête SQL (sélectionnez tous les enregistrements de maTable)
+$requete = $bdd->query('SELECT * FROM maTable');
+
+// Traitement des résultats
+while ($donnees = $requete->fetch()) {
+	// Utilisez $donnees pour accéder aux colonnes de chaque ligne
+	echo 'Colonne1 : ' . $donnees['colonne1'] . '<br>';
+	echo 'Colonne2 : ' . $donnees['colonne2'] . '<br>';
+	// ... Ajoutez d'autres colonnes selon votre structure de base de données
+}
+
+// Fermer la requête
+$requete->closeCursor();
 ?>
