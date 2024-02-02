097c42c18c3a93fc67cf9ca11f72f8220459a1d3
EnzoVallelian <enzo.vallelian@gmail.com>
2024-02-02 08:45:25 +0100

    changement de hiérarchie de fichier

---
+++ exercices/exercice4/serveur/serveur.php
@@ -0,0 +1,8 @@
+<?php
+			echo '<equipes>';
+			echo '<equipe><id>3</id><nom>SC Bern</nom></equipe>';
+			echo '<equipe><id>4</id><nom>Fribourg-Gottéron</nom></equipe>';
+			echo '<equipe><id>7</id><nom>HC Davos</nom></equipe>';
+			echo '<equipe><id>8</id><nom>Genève-Servette</nom></equipe>';
+			echo '</equipes>';
+?>
