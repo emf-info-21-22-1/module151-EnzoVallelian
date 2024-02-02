4f7fe39ab4b2168ac2de445fa562e3448d8c4933
EnzoVallelian <enzo.vallelian@gmail.com>
2024-02-02 15:48:02 +0100

    debut exo 8

---
+++ exercices/exercice8/serveur/BEANS/equipe.php
@@ -0,0 +1,14 @@
+<?php
+
+
+if ($_GET['action'] == "equipe") {
+	echo '<equipes>';
+	echo '<equipe><id>3</id><nom>SC Bern</nom></equipe>';
+	echo '<equipe><id>4</id><nom>Fribourg-Gotteron</nom></equipe>';
+	echo '<equipe><id>7</id><nom>HC Davos</nom></equipe>';
+	echo '<equipe><id>8</id><nom>Geneve-Servette</nom></equipe>';
+	echo '</equipes>';
+}
+
+
+?>
