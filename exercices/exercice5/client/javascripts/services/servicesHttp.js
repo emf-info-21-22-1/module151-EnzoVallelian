c6c62bcae52d7825309f2b0722535bff186d4714
EnzoVallelian <enzo.vallelian@gmail.com>
2024-02-02 09:22:32 +0100

    debut exo 5

--- exercices/exercice5/client/javascripts/services/servicesHttp.js
+++ exercices/exercice5/client/javascripts/services/servicesHttp.js
@@ -5,7 +5,7 @@
  * @version 1.0 / 20-SEP-2013
  */
 
-var BASE_URL = "METTEZ L'URL D'ADRESSE DU PHP DU SERVEUR";
+var BASE_URL = "http://localhost:8080/serveur/joueurs.php";
 
 /**
  * Fonction permettant de charger les données d'équipe.
